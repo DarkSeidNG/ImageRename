adf5dd15725fc7bde97dedf9e0a73f245a221efc
Ifeanyi Orji <chrisifwax101@gmail.com>
2019-03-05 11:17:47 +0100

    Added the Application file ImageRenameApplication

---
+++ src/main/java/image_rename/ImageRenameApplication.java
@@ -0,0 +1,12 @@
+package image_rename;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageRenameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImageRenameApplication.class, args);
+    }
+}

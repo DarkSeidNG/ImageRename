05fb798101542a5b121d49422783434edc108cdd
Ifeanyi Orji <chrisifwax101@gmail.com>
2019-03-05 11:05:07 +0100

    Added PinRecord model, this model will be used to access data on the database

---
+++ src/main/java/image_rename/models/PinRecord.java
@@ -0,0 +1,38 @@
+package image_rename.models;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class PinRecord {
+    @Id
+    private Integer id;
+
+    private Integer uniq_id;
+
+    private Integer pin;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getUniq_id() {
+        return uniq_id;
+    }
+
+    public void setUniq_id(Integer uniq_id) {
+        this.uniq_id = uniq_id;
+    }
+
+    public Integer getPin() {
+        return pin;
+    }
+
+    public void setPin(Integer pin) {
+        this.pin = pin;
+    }
+}

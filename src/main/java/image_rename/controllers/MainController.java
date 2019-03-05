670d911ef301fcf345ac97a087f60e5f806b090c
Ifeanyi Orji <chrisifwax101@gmail.com>
2019-03-05 12:28:17 +0100

    Added the rename image feature

--- src/main/java/image_rename/controllers/MainController.java
+++ src/main/java/image_rename/controllers/MainController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.io.File;
+
 @Controller
 @RequestMapping(path="/api")
 public class MainController {
@@ -17,6 +19,39 @@ public class MainController {
     @GetMapping(path="/renameAll")
     public @ResponseBody String renameImageFiles () {
 
+        File[] imageFiles = findImages();
+        if (imageFiles != null) {
+            for (final File imageFile : imageFiles) {
+                renameImage(imageFile);
+            }
+        }
         return "Saved";
     }
+
+    private File[] findImages() {
+        File dir = new File("/home/iorji/Pictures/test");
+
+        if (dir.isDirectory()) {
+            return dir.listFiles();
+        }
+        return null;
+    }
+
+    private void renameImage(File imageFile){
+        try {
+            String imagePin = imageFile.getName().replaceAll("[^0-9]+", "");
+            int imageId = pinRecordRepository.findByPin(Integer.parseInt(imagePin)).getUniq_id();
+            File renamedImage = new File("/home/iorji/Pictures/test/" + imageId + ".jpg");
+
+            if (imageFile.renameTo(renamedImage)) {
+                System.out.println("Rename succesful");
+            } else {
+                System.out.println("Rename failed");
+            }
+        } catch (Exception e) {
+            // TODO: handle exception
+            //e.printStackTrace();
+        }
+    }
+
 }
--- src/main/java/image_rename/repositories/PinRecordRepository.java
+++ src/main/java/image_rename/repositories/PinRecordRepository.java
@@ -4,5 +4,5 @@ import image_rename.models.PinRecord;
 import org.springframework.data.repository.CrudRepository;
 
 public interface PinRecordRepository extends CrudRepository<PinRecord, Integer> {
-
+    PinRecord findByPin(int pin);
 }
